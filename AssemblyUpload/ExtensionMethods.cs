dd4fa7e49b14b91d726b368e5b2c6e981202c8b3
David Forster <david@davidforster.com>
2015-07-19 12:54:12 +0100

    Adding an extension method and a constants class for readability/cleanliness

---
+++ AssemblyUpload/ExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using Tridion.ContentManager.CommunicationManagement;
+
+namespace DavidForster.Tridion.EventHandlers.AssemblyUpload
+{
+    static class ExtensionMethods
+    {
+        public static bool IsAssemblyTemplateBuildingBlock(this TemplateBuildingBlock templateBuildingBlock)
+        {
+            return templateBuildingBlock.TemplateType != TemplateTypes.AssemblyTemplate;
+        }
+    }
+}
---
+++ AssemblyUpload/TemplateTypes.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DavidForster.Tridion.EventHandlers.AssemblyUpload
+{
+    static class TemplateTypes
+    {
+        public const String AssemblyTemplate = "AssemblyTemplate";
+        public const String CSharpTemplate = "CSharpTemplate";
+    }
+}
