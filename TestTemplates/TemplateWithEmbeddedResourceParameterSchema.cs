a3332a4da50eb65866cbce2d859fc14b57b58b81
David Forster <david@davidforster.com>
2015-07-19 15:46:02 +0100

    Adding test template with embedded resource parameter schema

---
+++ TestTemplates/TemplateWithEmbeddedResourceParameterSchema.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Tridion.ContentManager.Templating;
+using Tridion.ContentManager.Templating.Assembly;
+
+namespace DavidForster.Tridion.EventHandlers.AssemblyUpload.TestTemplates
+{
+    [TcmTemplateParameterSchema("source:DavidForster.Tridion.EventHandlers.AssemblyUpload.TestTemplates.ParameterSchemas.Embedded Resource Paramer Schema.xsd")]
+    class TemplateWithEmbeddedResourceParameterSchema : ITemplate
+    {
+        public void Transform(Engine engine, Package package)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
